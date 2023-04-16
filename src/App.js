3513b280af7ecf2cac66fdffc6cf7e25b9a94d1f
Nuri <nuri1999sept@gmail.com>
2023-04-16 17:10:53 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import "./assets/Style/App.css";
-import Nuri from "./assets/Img/nuri.jpg";
-
+import AdvanceUSeState from "./component/AdvancedUseState";
+// import BasicUseState from "./component/BasicOfUseState";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="mahadev" src={Nuri}  alt="Nuri" />
-      </header>
-        <div className="App-intro">
-          <p>
-            This is the main repository of the project.Every hook components has
-            a unique branch named . just go to the that branch and run it .
-          </p>
-        </div>
-    </div>
+    // <BasicUseState/> 
+    <AdvanceUSeState/>
   );
 }
 
