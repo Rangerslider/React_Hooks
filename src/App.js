eacf6c6a2bf7da8e157659c4af6745376feffa9e
Nuri <nuri1999sept@gmail.com>
2023-04-17 21:13:56 +0600

    update component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
-
 import "./assets/Style/App.css";
-import CustomHookExample from "./components/UseCallback_UseMemo";
+// import AdvanceUseContext from "./components/AdvanceOfUseContext";
+// import UserContext from "./components/Using context component/UserContext";
+import component from "./components/Context-component/component";
+// import Component2 from "./components/ Context-component/Component2";
+// import Component3 from "./components/ Context-component/Component3";
+
+
 
 function App() {
   
   return (
     <>
-      <CustomHookExample/>
+      <components/>
     </>
   );
 }
